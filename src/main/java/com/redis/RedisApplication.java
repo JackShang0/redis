6c01a63a23ed283bde4fec5006c8d5306ae5ebff
shangdj <15091520295@163.com>
2024-07-04 16:00:54 +0800

    配置redis分片集群

--- src/main/java/com/redis/RedisApplication.java
+++ src/main/java/com/redis/RedisApplication.java
@@ -2,10 +2,10 @@ package com.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 
-
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class RedisApplication {
 	
 	public static void main(String[] args) {
